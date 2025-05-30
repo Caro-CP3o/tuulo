995f9db80898ebe197c7fd82f863a5d6d359b7b3
Cp3o <carolepes@gmail.com>
2025-05-30 11:49:40 +0200

    build: logout controller

--- src/Controller/LogoutController.php
+++ src/Controller/LogoutController.php
@@ -1,18 +1,34 @@
 <?php
 
+// src/Controller/LogoutController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
-final class LogoutController extends AbstractController
+class LogoutController extends AbstractController
 {
-    #[Route('/logout', name: 'app_logout')]
-    public function index(): Response
+    #[Route('/api/logout', name: 'api_logout', methods: ['POST'])]
+    public function logout(): Response
     {
-        return $this->render('logout/index.html.twig', [
-            'controller_name' => 'LogoutController',
-        ]);
+        $cookie = Cookie::create(
+            name: 'token',
+            value: '',
+            expire: time() - 3600, // Expired in the past
+            path: '/',
+            domain: null,
+            secure: false, // true in prod
+            httpOnly: true,
+            sameSite: Cookie::SAMESITE_LAX
+        );
+
+        $response = new JsonResponse(['message' => 'Logged out']);
+        $response->headers->setCookie($cookie);
+
+        return $response;
     }
 }
