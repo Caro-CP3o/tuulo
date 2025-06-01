2303b74bafc8b0ff6b3e5e0e162da601688271ca
Cp3o <carolepes@gmail.com>
2025-06-01 16:32:15 +0200

    build: automatic fields on posts subscriber

---
+++ src/Events/PostAuthorSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Symfony\EventListener\EventPriorities;
+use App\Entity\Post;
+use App\Repository\FamilyMemberRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class PostAuthorSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private Security $security,
+        private FamilyMemberRepository $familyMemberRepo
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::VIEW => ['setAuthorAndFamily', EventPriorities::PRE_VALIDATE],
+        ];
+    }
+
+    public function setAuthorAndFamily(ViewEvent $event): void
+    {
+        $post = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$post instanceof Post || $method !== 'POST') {
+            return;
+        }
+
+        $user = $this->security->getUser();
+
+        if ($user) {
+            $post->setAuthor($user);
+
+            $familyMember = $this->familyMemberRepo->findOneBy(['user' => $user]);
+            if ($familyMember) {
+                $post->setFamily($familyMember->getFamily());
+            }
+        }
+        foreach ($post->getImages() as $image) {
+            $image->setPost($post);
+        }
+    }
+}
